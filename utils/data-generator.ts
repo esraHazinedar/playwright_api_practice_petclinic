1a89e08a60803b635a805b13a6146f3ab952f0ba
esraHazinedar <esra.bengu.hazinedar@gmail.com>
2026-05-07 22:23:01 +0300

    added the missing second test and did the suggested changes

--- utils/data-generator.ts
+++ utils/data-generator.ts
@@ -1,10 +1,20 @@
-import petRequestJson from "../request-objects/POST-pet.json"
+import { stringify } from "node:querystring"
+import petTypeRequestJson from "../request-objects/POST-petType.json"
+import petRequestJson from "../request-objects/POST_pet.json"
+
 import{faker} from '@faker-js/faker'
 
-export function getNewPet(){
-    const petRequest = structuredClone(petRequestJson)
-    petRequest.name = faker.animal.horse()
-    return petRequest
+export function createRandomPetType(){ 
+    const petTypeRequest = structuredClone(petTypeRequestJson)
+    petTypeRequest.name = faker.animal.horse()
+    return petTypeRequest
 }
 
 
+export function createRandomPet(){
+    const petRequest = structuredClone(petRequestJson)
+    petRequest.name = faker.person.firstName()
+  
+    
+    return petRequest
+}

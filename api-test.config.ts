26e599f74397d3f38bc3dbb7984e075cfedbc143
esraHazinedar <esra.bengu.hazinedar@gmail.com>
2026-09-19 14:20:29 +0300

    chore: remove unrelated formatting changes

--- api-test.config.ts
+++ api-test.config.ts
@@ -1,19 +1,21 @@
-const processENV = process.env.TEST_ENV;
-const env = processENV || "dev";
-console.log("Test environment is: " + env);
+const processENV = process.env.TEST_ENV
+const env = processENV || 'dev'
+console.log('Test environment is: ' + env)
 
 const config = {
-  apiUrl: "https://petclinic-api.bondaracademy.com/petclinic/api",
-  userEmail: process.env.USERNAME as string,
-  userPassword: process.env.PASSWORD as string,
-};
-
-if (env === "qa") {
-  ((config.userEmail = process.env.USERNAME as string),
-    (config.userPassword = process.env.PASSWORD as string));
-}
-if (env === "prod") {
-  ((config.userEmail = ""), (config.userPassword = ""));
+    apiUrl: 'https://petclinic-api.bondaracademy.com/petclinic/api',
+    userEmail: process.env.USERNAME as string,
+    userPassword: process.env.PASSWORD as string
 }
 
-export { config };
+if(env === 'qa'){
+    config.userEmail = process.env.USERNAME as string,
+    config.userPassword = process.env.PASSWORD as string
+}
+if(env === 'prod'){
+    config.userEmail = '',
+    config.userPassword = ''
+}
+
+
+export {config}
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig, devices } from "@playwright/test";
-import dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
 
 export default defineConfig({
-  testDir: "./tests",
+  testDir: './tests',
   fullyParallel: false,
   retries: process.env.CI ? 2 : 1,
   workers: 1,
-  reporter: [["html"], ["list"]],
+  reporter: [['html'], ['list']],
   use: {
-    trace: "retain-on-failure",
+    trace: 'retain-on-failure'
   },
 
   projects: [
     {
-      name: "api-testing",
-    },
+      name: 'api-testing'
+    }
   ],
 });
